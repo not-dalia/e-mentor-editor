b1abab502bc34c42419fadbce1c5c613225ae50c
Dalya <LocalAdmin@16DCompL069.campus.ncl.ac.uk>
2018-11-05 17:26:52 +0000

    fixed another bug

--- app.js
+++ app.js
@@ -36,8 +36,8 @@ app.use(logger("dev"));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: false, limit: "50mb" }));
 app.use(cookieParser());
-app.use(process.env.BASE_URL, stylus.middleware(path.join(__dirname, "public")));
-app.use(process.env.BASE_URL, express.static(path.join(__dirname, "public")));
+app.use(stylus.middleware(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
 
 var router = express.Router();
 
